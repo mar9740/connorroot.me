1b7438e28e00fec555ae5dcb6a393079bfb2cf43
Root, Michael <michael.root@rovisys.com>
2019-03-05 16:18:42 -0500

    added react app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Text>Hey Connor</Text>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> to alter certain things why won't this work.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
