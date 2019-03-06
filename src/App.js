e996342e04cfcbe81ab033a5bad969b116101d4c
Root, Michael <michael.root@rovisys.com>
2019-03-06 16:51:41 -0500

    Fixed home tab

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Container } from 'semantic-ui-react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import CustomHeader from './Components/CustomHeader';
-import Home from './Pages/HomePage';
+import HomePage from './Pages/HomePage';
 import About from './Pages/About';
 import Baseball from './Pages/Baseball';
 
@@ -15,7 +15,7 @@ class App extends Component {
           <Router>
             <div>
               <CustomHeader/>
-                <Route exact path='/' component={Home}/>
+                <Route exact path='/' component={HomePage}/>
                 <Route exact path='/about' component={About}/>
                 <Route exact path='/baseball' component={Baseball}/>
             </div>
