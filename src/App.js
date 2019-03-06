d13a9c04b669710d29da1ba6528443ada1af646b
Root, Michael <michael.root@rovisys.com>
2019-03-06 10:03:25 -0500

    connor number 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -6,10 +7,18 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit to actualy make this shit work connor 
+            Edit <code>src/App.js</code> to alter certain things why won't this work.
           </p>
-
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
