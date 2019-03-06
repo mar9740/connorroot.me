c05f230e17c8d94600fd21e67f1975b76cf65594
Root, Michael <michael.root@rovisys.com>
2019-03-06 16:58:01 -0500

    Fixed home tab again

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { Container, Header } from 'semantic-ui-react';
 
 class HomePage extends Component{
 	render(){
 		return(
 			<div>
-				HomePage
+				<Container fluid>
+					<Header as='h2'>Home Page</Header>
+					<p>
+					Welcome! This is my personal website to display all my accomplishments and info to the
+					general populace.
+					</p>
+					<p>
+					Filler stuff here
+					</p>
+					<p>
+					Extra break
+					</p>
+				</Container>
 			</div>
 		);
 	}
